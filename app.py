e507d6a59788c4fd7bac6a856ca35d7146c2ca83
MyVeli <veli.myllyla@hotmail.com>
2021-04-10 09:28:35 +0300

    added registration and login

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
+#from flask_session import Session
 from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+#Session(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    print("testi")
+    if session.get("username") == None:
+        return redirect("/login")
     books = db.session.execute("SELECT author,name FROM Title")
     return render_template("index.html",message="Tervetuloa!",items=books)
 
@@ -42,30 +46,46 @@ def add_book(author,name,genre,status,owner_id):
     db.session.execute(query,{"author":author,"name":name,"genre":genre,"status":status,"owner_id":owner_id})
     db.session.commit()
 
-@app.route("/login",methods=["POST"])
+@app.route("/login")
 def login():
+    return render_template("login.html")
+
+@app.route("/verify_credentials",methods=["POST"])
+def verify_credentials():
     username = request.form["username"]
     pw = request.form["pw"]
-    
-    session["username"] = username
-    return
+    query = "SELECT password FROM Users WHERE name=:username"
+    pw_hash = db.session.execute(query,{"username":username}).fetchone()[0]
+    if pw_hash == None:
+        return render_template("login.html",message="Wrong password or username.")
+    elif check_password_hash(pw_hash,pw):
+        session["username"] = username
+    else:
+        print( check_password_hash(pw_hash,pw))
+        return render_template("login.html",message="Wrong password or username.")
+    return redirect("/")
 
-@app.route("/register",methods=["POST"])
+@app.route("/register")
 def register():
-    return
+    return render_template("register.html")
 
 @app.route("/logout")
 def logout():
     del session["username"]
-    return redirect("/")
+    return redirect("/login")
 
-@app.route("/adduser",methods=["POST"])
+@app.route("/add_user",methods=["POST"])
 def add_user():
-    username = request.form("username")
-    pw = generate_password_hash(request.form("password"))
+    username = request.form["username"]
+    pw = generate_password_hash(request.form["pw"])
     query = "SELECT id FROM Users Where name=:username"
-    if db.session.execute(query, {"username":username}) != None:
-        return redirect("/register")
+    if db.session.execute(query, {"username":username}).fetchone() != None:
+        return render_template("register.html",message="User name is already in use. Please choose another one")
     else:
-        query = "INSERT INTO Users (name, password) VALUES {:username,:pw}"
-        db.session.execute(query, {"username":username,"password":pw})
+        query = "INSERT INTO Users (name, password) VALUES (:name,:pw)"
+        try:
+            db.session.execute(query, {"name":username,"pw":pw})
+            db.session.commit()
+        except Exception:
+            print("virhe")
+    return render_template("login.html",message="Account created. Please login with the new credentials.")
--- database_schema.sql
+++ database_schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE BookStatus(
 
 CREATE TABLE Users(
     id SERIAL PRIMARY KEY,
-    name TEXT,
+    name TEXT UNIQUE,
     password TEXT 
 );
 
